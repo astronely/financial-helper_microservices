4a6ee062cd4a79cce710f345739e3b4120502028
astronely <nikborowik200394@gmail.com>
2025-02-17 17:13:38 +0500

    fix: create_test.go fixed add: token config mock

---
+++ internal/config/generate.go
@@ -0,0 +1,4 @@
+package config
+
+//go:generate sh -c "rm -rf mocks && mkdir -p mocks"
+//go:generate mockery --name TokenConfig --output ./mocks
--- internal/service/user/tests/create_test.go
+++ internal/service/user/tests/create_test.go
@@ -3,18 +3,23 @@ package tests
 import (
 	"context"
 	"fmt"
+	"github.com/astronely/financial-helper_microservices/internal/config"
+	tokenMocks "github.com/astronely/financial-helper_microservices/internal/config/mocks"
 	"github.com/astronely/financial-helper_microservices/internal/model"
 	"github.com/astronely/financial-helper_microservices/internal/repository"
 	"github.com/astronely/financial-helper_microservices/internal/repository/mocks"
 	"github.com/astronely/financial-helper_microservices/internal/service/user"
 	"github.com/astronely/financial-helper_microservices/internal/utils"
 	"github.com/brianvoe/gofakeit/v7"
+	mockLib "github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func Test_serv_Create(t *testing.T) {
 	type userRepositoryMockFunc func() repository.UserRepository
+	type tokenConfigMockFunc func() config.TokenConfig
 
 	type args struct {
 		ctx      context.Context
@@ -31,7 +36,7 @@ func Test_serv_Create(t *testing.T) {
 			model.UserInfo{
 				Name:  name,
 				Email: email,
-			}, []byte("testSecretKey"), 360)
+			}, []byte("testSecretKey"), 360*time.Second)
 		password = gofakeit.Password(true, true, true, true, false, 10)
 		info     = &model.UserInfo{
 			Email: email,
@@ -48,6 +53,7 @@ func Test_serv_Create(t *testing.T) {
 		want1              string
 		err                error
 		userRepositoryMock userRepositoryMockFunc
+		tokenConfigMock    tokenConfigMockFunc
 	}{
 		{
 			name: "success case",
@@ -64,6 +70,12 @@ func Test_serv_Create(t *testing.T) {
 				mock.On("Create", ctx, info, password).Return(id, nil)
 				return mock
 			},
+			tokenConfigMock: func() config.TokenConfig {
+				mock := tokenMocks.NewTokenConfig(t)
+				mock.On("RefreshTokenKey").Return("testSecretKey")
+				mock.On("RefreshTokenExpirationTime").Return(time.Duration(360 * time.Second))
+				return mock
+			},
 		},
 		{
 			name: "error case",
@@ -77,26 +89,34 @@ func Test_serv_Create(t *testing.T) {
 			want1: "",
 			userRepositoryMock: func() repository.UserRepository {
 				mock := mocks.NewUserRepository(t)
-				mock.On("Create", ctx, info, password).Return(int64(0), repoErr)
+				mock.On("Create", mockLib.Anything, info, password).Return(int64(0), repoErr)
 				return mock
+			},
+			tokenConfigMock: func() config.TokenConfig {
+				return nil
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			userRepositoryMock := tt.userRepositoryMock()
+			tokenConfigMock := tt.tokenConfigMock()
+
 			defer func() {
 				if mock, ok := userRepositoryMock.(*mocks.UserRepository); ok {
 					mock.AssertExpectations(t)
 				}
+				if tokenMock, ok := tokenConfigMock.(*tokenMocks.TokenConfig); ok {
+					tokenMock.AssertExpectations(t)
+				}
 			}()
 
-			service := user.NewMockService(userRepositoryMock)
+			service := user.NewMockService(userRepositoryMock, tokenConfigMock)
 
 			resID, resToken, err := service.Create(tt.args.ctx, tt.args.info, tt.args.password)
 			require.Equal(t, tt.err, err)
-			require.Equal(t, tt.want, resID)
-			require.Equal(t, tt.want1, resToken)
+			require.Equal(t, tt.want, resID, "Id dont match")
+			require.Equal(t, tt.want1, resToken, "Token dont match")
 		})
 	}
 }
